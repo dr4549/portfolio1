8da1792fb2ebffa602e84680518c3ca2f7365416
dr4549 <89069633+dr4549@users.noreply.github.com>
2023-10-15 16:49:00 +0900

    홈 수정

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@
 const header = document.querySelector('.header');
 const headerHeight =header.getBoundingClientRect().height;
 document.addEventListener('scroll',() =>{
-    console.log(window.scrollY);
+    //Header에 페이지 아래로 스크롤시 다크 스타일링 적용
     if(window.scrollY > headerHeight ) {
         header.classList.add('header--dark');
     } else {
         header.classsList.remove('header--dark');
     }
 })
+
+//Home 섹션을 아래로 스크롤시 투명하게처리함
+const home =document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll',()=> {
+    console.log(1 - window.scroolY/ homeHeight);
+    home.style.opacity = 1 - window.scroolY/ homeHeight;
+});
